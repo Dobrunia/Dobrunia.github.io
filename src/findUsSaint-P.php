a5605f666617e752dde05dd881b7158bfe873543
Dobrunia <48620984+Dobrunia@users.noreply.github.com>
2020-08-15 17:37:49 +0300

    Add files via upload

---
+++ src/findUsSaint-P.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ekaterina Kostrigina</title>
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <?php
+        include('navbar.php');
+    ?>
+        <div id="maps">
+            <div class="map">
+                <div>
+                    <h2>Сокровища Петербурга:</h2>
+                    <h3>+7 921 9476004</h3></div>
+                <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Acce1b954eb0714884c55f8732df63fe74e0de7b0a7e294525e9b08a9558cd236&amp;width=320&amp;height=240&amp;lang=ru_RU&amp;scroll=false"></script>
+            </div>
+            <div class="map">
+                <div>
+                    <h2>Магазин в Гранд Паласе:</h2>
+                    <h3>+7 921 9476004</h3></div>
+                <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A28ce5d59a69c274f338e1b255a22b7b890e11a6f3b900b0b78996c49084e2d56&amp;width=320&amp;height=240&amp;lang=ru_RU&amp;scroll=false"></script>
+            </div>
+            <div class="map">
+                <div>
+                    <h2>Магазин на Монетной 23:</h2>
+                    <h3>+7 921 9476004</h3></div>
+                <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A8442b2c336bf53bccc5d89ddcc07a0f2c7e80b9736222e222dfa3a7a757beb6a&amp;width=320&amp;height=240&amp;lang=ru_RU&amp;scroll=false"></script>
+            </div>
+            <br>
+        </div>
+        <script src="./js/scripts.js" type=""></script>
+</body>
+
+</html>
---
+++ src/navbar.php
@@ -0,0 +1,28 @@
+<div class="menuToggle" id="Toggle">|||</div>
+<header id='header' class="">
+    <a href="index.php">
+        <button class="btn">главная</button>
+    </a>
+    <div>
+        <button id="Toggle2" class="btn">ювелирные изделия</button>
+        <nav id="products" class="disp">
+            <button class="btn">все</button>
+            <button class="btn">золото</button>
+            <button class="btn">броши</button>
+            <button class="btn">серьги</button>
+            <button class="btn">подвески</button>
+            <button class="btn">браслеты</button>
+            <button class="btn">кольца</button>
+            <button class="btn">колье</button>
+            <button class="btn">бусы</button>
+        </nav>
+    </div>
+    <button class="btn">о нас</button>
+    <div>
+        <button id="Toggle3" class="btn">как нас найти</button>
+        <nav id="find" class="disp">
+            <a href="findUsMoscow.php"><button class="btn">Москва</button></a>
+            <a href="findUsSaint-P.php"><button class="btn">Санкт-Петербург</button></a>
+        </nav>
+    </div>
+</header>
